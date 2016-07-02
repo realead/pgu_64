064748943c97d26f361c6290f087d6bd9b5cf83a
egor.dranischnikow <egor.dranischnikow@googlemail.com>
2016-07-02 09:34:06 +0200

    adding script for building and running examples

---
+++ examples/build_and_run.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# run "sh build_and_run.sh XXX" or  "sh build_and_run.sh XXX 64"  for building, running and cleaning up example XXX in 64 bit mode
+# run "sh build_and_run.sh XXX 32"  for building, running and cleaning up example XXX in 32 bit mode 
+
+
+
+########## ANALISE OPTIONS ###############:
+ARCH=${2:-"64"}
+TARGET=$1/$1$ARCH
+SOURCE=$TARGET.s
+OBJECT=$TARGET.o
+
+   
+########### BUILD ####################:
+
+#as in the book:
+if [ $ARCH = "32" ] ; then
+    LD_FLAG="-melf_i386"
+else
+    LD_FLAG="-melf_x86_64"
+fi    
+ 
+as --$ARCH $SOURCE -o $OBJECT
+ld $LD_FLAG $OBJECT -o $TARGET
+
+    
+# another possibility:
+#gcc -nostdlib -m$ARCH $SOURCE -o $TARGET 
+
+############ RUN ###################:
+./$TARGET
+echo "$TARGET : $?"
+
+
+############## CLEAN UP ########################:
+rm $OBJECT
+rm $TARGET
+
